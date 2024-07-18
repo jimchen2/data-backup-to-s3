f97474509d8fd5c5ef2fe36868e9f4b73286f3a1
jimchen2 <jimchen4214@gmail.com>
2024-07-18 08:50:00 +0800

    Updates

--- backup-github.sh
+++ backup-github.sh
@@ -9,6 +9,11 @@ TOKEN="$1"
 S3_BUCKET="$2"
 TIMESTAMP=$(date +%Y%m%d_%H%M%S)
 ZIPNAME="github_repos_$TIMESTAMP.zip"
+TEMP_DIR="github_repos_$TIMESTAMP"
+
+# Create temporary directory
+mkdir "$TEMP_DIR"
+cd "$TEMP_DIR" || exit 1
 
 # Clone repositories
 curl -s -H "Authorization: token $TOKEN" "https://api.github.com/user/repos?per_page=100" | \
@@ -17,8 +22,11 @@ while read repo; do
     git clone "https://${TOKEN}@github.com/${repo#https://github.com/}"
 done
 
+# Go back to parent directory
+cd ..
+
 # Zip repositories
-zip -r "$ZIPNAME" *
+zip -r "$ZIPNAME" "$TEMP_DIR"
 
 # Upload to S3
 aws s3 cp "$ZIPNAME" "s3://$S3_BUCKET/$ZIPNAME"
@@ -26,4 +34,5 @@ aws s3 cp "$ZIPNAME" "s3://$S3_BUCKET/$ZIPNAME"
 echo "All repositories cloned, zipped to $ZIPNAME, and uploaded to S3 bucket $S3_BUCKET"
 
 # Clean up
+rm -rf "$TEMP_DIR"
 rm "$ZIPNAME"
